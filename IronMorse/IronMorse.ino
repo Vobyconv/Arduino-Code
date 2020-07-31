2938e197e551aef8d916cb26b9eba2ce0e95b238
Roberto Rodriguez <47628679+Vobyconv@users.noreply.github.com>
2020-07-31 08:39:40 +0200

    Update IronMorse.ino

--- IronMorse/IronMorse.ino
+++ IronMorse/IronMorse.ino
@@ -1,9 +1,15 @@
 #include <Adafruit_NeoPixel.h>
 
 int tira1 = 3;
+int tira2 = 4;
+int tira5 = 5;
 int puntosled = 4;
+int puntosheart1 = 6;
+int puntosheart2 = 15;
 
 Adafruit_NeoPixel strip1 = Adafruit_NeoPixel(puntosled, tira1, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip2 = Adafruit_NeoPixel(puntosheart1, tira2, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel strip5 = Adafruit_NeoPixel(puntosheart2, tira5, NEO_GRB + NEO_KHZ800);
 
 int mediluz = A1;
 int medipot = A2;
@@ -11,12 +17,26 @@ int medipot = A2;
 int corto = 250;
 int largo = 750;
 int espera = 1000;
+int vuelta = 20;
+int pulsar = 35;
+int bombeo = 11;
+int factor = 4;
 
 void iniled(){
   strip1.begin();
   strip1.setBrightness(200);
   strip1.show();
   strip1.clear();
+
+  strip2.begin();
+  strip2.setBrightness(200);
+  strip2.show();
+  strip2.clear();
+
+  strip5.begin();
+  strip5.setBrightness(200);
+  strip5.show();
+  strip5.clear();
 }
 
 void a(){punto(); raya(); delay(espera);}
@@ -69,8 +89,8 @@ void raya(){
 }
 
 void start(){
- for (int i = 0; i <= puntosled; i++) {
-  strip1.setPixelColor(i, 150+i*20, 1, 1);
+ for (int i = 0; i <= 10; i++) {
+  strip1.setPixelColor(i, 150+i*10, 1, 1);
   strip1.show();
  }
   delay(1000);
@@ -79,16 +99,55 @@ void start(){
   delay(1000);
 }
 
+void beat(){
+  for (int i = 0; i <= bombeo ; i++) {
+      strip2.setPixelColor(i, 50+i*factor, 50+i*factor, 50+i*factor+20);
+      strip2.show();
+      delay(pulsar);
+ }
+  
+ for (int i = 0; i <= bombeo ; i++) {
+      strip2.setPixelColor(i, 255-i*factor, 255-i*factor, 255-i*factor-20);
+      strip2.show();
+      delay(pulsar);
+      }
+ Serial.println("palpitar");
+}
+
+void beatone(){
+  for (int i = 0; i <= bombeo ; i++) {
+      strip2.setPixelColor(i, 50+i*factor, 50+i*factor, 50+i*factor+20);
+      strip2.show();
+      delay(pulsar);
+ }
+
+ Serial.println("palpitar");
+}
+
+void snake(){
+  for (int i = 0; i <= puntosheart2 ; i++) {
+      strip5.setPixelColor(i, 210, 210, 255);
+      strip5.show();
+      delay(vuelta);
+      strip5.setPixelColor(i, 130, 130, 160);
+      strip5.show();
+      delay(vuelta);
+    
+      }
+    Serial.println("lucecicas");
+
+}
+
 void sensorluz(){
  int poten = analogRead(A2); 
- int medipot = map(poten, 0, 1024, 0, 20);
+ int medipot = map(poten, 0, 1023, 0, 20);
   Serial.print("{potpoten: ");
   Serial.print(poten);
   Serial.print("/ sensibilidad: ");
   Serial.println(medipot);
   
  int mediluz = analogRead(A1); 
- int medipos = map(mediluz, 900, 1020, 0, 20);
+ int medipos = map(mediluz, 300, 800, 0, 20);
   Serial.print("} {luz: ");
   Serial.print(mediluz);
   Serial.print("/ medida: ");
@@ -96,7 +155,7 @@ void sensorluz(){
 
   if (medipos > medipot){
   //un parpadeo rojo de inicio
-       start();
+    start();
   //emision   
       j(); Serial.print("- j -");
       a(); Serial.print(" a -");
@@ -109,16 +168,19 @@ void sensorluz(){
      strip1.clear();
      strip1.show();  
      }
-     delay(500);
+     delay(5);
 }
+
 
 void setup(){
   Serial.begin(9600);
   Serial.println("+++ Industrias Stark +++ iniciando a jarvis ...");
   iniled();
+  beatone();
 }
 
-void loop()
-{
-sensorluz();
+void loop(){
+  beat();
+  snake();
+  sensorluz();
 }
