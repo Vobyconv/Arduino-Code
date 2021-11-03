e2b5d874ea5273ed290ed2ccece7a446804c42a0
Andres Garcia Mangas <andres.garcia@fundacionctic.org>
2021-11-03 13:07:05 +0100

    Poner solucion definitiva

--- Dracula/Velas/src/main.cpp
+++ Dracula/Velas/src/main.cpp
@@ -7,24 +7,47 @@ const uint32_t DEFAULT_COLOR = Adafruit_NeoPixel::Color(255, 255, 0);
 
 Adafruit_NeoPixel ledStrip = Adafruit_NeoPixel(LED_NUM, LED_PIN, NEO_RGB + NEO_KHZ800);
 
+const int RELAY_PIN = 2;
+
 const int NUM_BUTTONS = 5;
+const int BUTTON_PINS[NUM_BUTTONS] = {4, 5, 6, 7, 8};
+const int RESET_BUTTON_PIN = 12;
 
 Atm_button buttons[NUM_BUTTONS];
-
-const int BUTTON_PINS[NUM_BUTTONS] = {2, 3, 4, 5, 6};
+Atm_button resetButton;
 
 const bool BUTTON_CONFIGURATION[NUM_BUTTONS][LED_NUM] = {
-    {true, false, false, false, true, false, false, false, true, true},
-    {false, false, false, false, true, false, true, false, true, true},
-    {true, false, false, true, true, false, false, false, false, true},
-    {false, false, true, false, true, false, false, false, false, false},
-    {true, false, false, false, true, false, true, false, true, false}};
+    {true, false, true, true, false, true, true, true, false, true},
+    {false, false, true, false, false, true, true, true, false, false},
+    {true, false, true, false, false, false, false, false, false, false},
+    {true, true, false, false, false, false, false, false, false, false},
+    {true, true, true, true, true, true, true, true, true, true}};
 
 const bool VALID_CONFIGURATION[LED_NUM] = {
-    false, false, false, false, false, false, false, true, false, true};
+    false, false, true, false, true, false, false, false, true, false};
 
 bool currConfiguration[LED_NUM] = {
-    false, false, false, false, false, false, false, false, false, false};
+    true, true, true, true, true, true, true, true, true, true};
+
+bool flagRelayOpen = false;
+
+void lockRelay()
+{
+  digitalWrite(RELAY_PIN, LOW);
+  flagRelayOpen = false;
+}
+
+void openRelay()
+{
+  digitalWrite(RELAY_PIN, HIGH);
+  flagRelayOpen = true;
+}
+
+void initRelay()
+{
+  pinMode(RELAY_PIN, OUTPUT);
+  lockRelay();
+}
 
 void initLeds()
 {
@@ -47,6 +70,28 @@ void updateLeds()
   ledStrip.show();
 }
 
+void blinkLeds()
+{
+  const int numLoops = 6;
+  const int delayLoop = 150;
+
+  ledStrip.clear();
+  ledStrip.show();
+
+  for (int i = 0; i < numLoops; i++)
+  {
+    ledStrip.fill(DEFAULT_COLOR);
+    ledStrip.show();
+    delay(delayLoop);
+    ledStrip.clear();
+    ledStrip.show();
+    delay(delayLoop);
+  }
+
+  ledStrip.clear();
+  ledStrip.show();
+}
+
 bool isCurrentConfigValid()
 {
   for (int idxLed = 0; idxLed < LED_NUM; idxLed++)
@@ -60,11 +105,12 @@ bool isCurrentConfigValid()
   return true;
 }
 
-void checkConfig()
+void checkState()
 {
-  if (isCurrentConfigValid())
+  if (isCurrentConfigValid() && !flagRelayOpen)
   {
-    Serial.println(F("Valid config"));
+    Serial.println(F("Valid config: opening relay"));
+    openRelay();
   }
 }
 
@@ -82,12 +128,24 @@ void onPress(int idx, int v, int up)
   }
 }
 
+void onReset(int idx, int v, int up)
+{
+  Serial.println(F("Reset"));
+
+  for (int idxLed = 0; idxLed < LED_NUM; idxLed++)
+  {
+    currConfiguration[idxLed] = true;
+  }
+}
+
 void initButtons()
 {
   for (int i = 0; i < NUM_BUTTONS; i++)
   {
     buttons[i].begin(BUTTON_PINS[i]).onPress(onPress, i);
   }
+
+  resetButton.begin(RESET_BUTTON_PIN).onPress(onReset);
 }
 
 void setup()
@@ -95,6 +153,8 @@ void setup()
   Serial.begin(9600);
   initLeds();
   initButtons();
+  initRelay();
+  blinkLeds();
   Serial.println(F("Velas"));
 }
 
@@ -102,5 +162,5 @@ void loop()
 {
   automaton.run();
   updateLeds();
-  checkConfig();
+  checkState();
 }
